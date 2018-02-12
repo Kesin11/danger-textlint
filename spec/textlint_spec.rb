3c31ec1312b61b697abc17510639c50cd22fbf3b
Kenta Kase <kesin1202000@gmail.com>
2018-02-12 14:39:56 +0900

    Fix spec file

--- spec/textlint_spec.rb
+++ spec/textlint_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require File.expand_path("../spec_helper", __FILE__)
 
 module Danger
@@ -14,28 +16,20 @@ module Danger
         @dangerfile = testing_dangerfile
         @my_plugin = @dangerfile.textlint
       end
+    end
 
-      # Some examples for writing tests
-      # You should replace these with your own.
-
-      it "Warns on a monday" do
-        monday_date = Date.parse("2016-07-11")
-        allow(Date).to receive(:today).and_return monday_date
-
-        @my_plugin.warn_on_mondays
-
-        expect(@dangerfile.status_report[:warnings]).to eq(["Trying to merge code on a Monday"])
+    describe "textlint config" do
+      describe ".target_path" do
       end
 
-      it "Does nothing on a tuesday" do
-        monday_date = Date.parse("2016-07-12")
-        allow(Date).to receive(:today).and_return monday_date
-
-        @my_plugin.warn_on_mondays
-
-        expect(@dangerfile.status_report[:warnings]).to eq([])
+      describe ".config_file" do
       end
+    end
 
+    describe ".send_inline_comment" do
+    end
+
+    describe ".lint" do
     end
   end
 end
