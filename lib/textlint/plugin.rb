81c266eb49b255e6eb8194df3c1f49d180009b81
Kenta Kase <kesin1202000@gmail.com>
2018-02-12 23:19:48 +0900

    [ci skip] Fix document

--- lib/textlint/plugin.rb
+++ lib/textlint/plugin.rb
@@ -2,15 +2,7 @@ require "mkmf"
 require "json"
 
 module Danger
-  # This is your plugin class. Any attributes or methods you expose here will
-  # be available from within your Dangerfile.
-  #
-  # To be published on the Danger plugins site, you will need to have
-  # the public interface documented. Danger uses [YARD](http://yardoc.org/)
-  # for generating documentation from your plugin source, and you can verify
-  # by running `danger plugins lint` or `bundle exec rake spec`.
-  #
-  # You should replace these comments with a public description of your library.
+  # [Danger](http://danger.systems/ruby/) plugin for [textlint](https://textlint.github.io/).
   #
   # @example Run textlint and send violations as inline comment.
   #
